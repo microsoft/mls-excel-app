f7c231937401e7ef38a035811b7c98d27f937bc4
Sean Wells <seanw@microsoft.com>
2017-09-13 06:02:48 -0700

    fix(auth): [PenTest]
    [PenTest] suggestions
    
    BREAKING CHANGE: Removed `Remember Me` functionality
    
    #83962

--- src/app/auth/auth-admin/auth-admin.component.ts
+++ src/app/auth/auth-admin/auth-admin.component.ts
@@ -20,46 +20,24 @@ export class AuthAdminComponent implements OnInit {
   };
   public error: boolean;
   public errorMessage: string;
-  public rememberMe: boolean;
 
   constructor(private authService: AuthService, private router: Router) {
     this.error = false;
   }
 
   ngOnInit() {
-    // Checks to see if the user credentials have been stored locally via 'remember me'
-    // Populates them in the form if they exist
-    if (localStorage.getItem('adminUser')) {
-      const data = JSON.parse(localStorage.getItem('adminUser'));
-      this.config = {
-        connection: data.connection,
-        username: data.username,
-        password: data.password
-      };
-      this.rememberMe = true;
-    } else {
-      this.config = {
-        connection: '',
-        username: '',
-        password: ''
-      };
-      this.rememberMe = false;
-    }
+    this.config = {
+      connection: '',
+      username: '',
+      password: ''
+    };
   }
 
   // Called when the user submits for authentication
-  // Stores credentials locally if user checked the 'Remember Me'
   // Sends a HTTP GET to the specified connection
   // If authentication is successful, reroutes user to web-services
   // Otherwise, user is shown an appropriate error message
   onSubmit(form: NgForm) {
-    // Store credentials if user wants to be remembered
-    if (form.value.remember) {
-      localStorage.setItem('adminUser',
-        JSON.stringify({ connection: form.value.connection, username: form.value.username, password: form.value.password }));
-    } else {
-      localStorage.removeItem('adminUser');
-    }
     this.authService.loginAdmin(form.value.connection, form.value.username, form.value.password)
       .subscribe(
       (data: any) => {
